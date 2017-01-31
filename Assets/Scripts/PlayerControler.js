348a5cba41d1c13d2ed863ddd4ae97ccc27145a1
melbandit <melissa.i.sattler@gmail.com>
2017-01-31 08:42:29 -0500

    updates

--- Assets/Scripts/PlayerControler.js
+++ Assets/Scripts/PlayerControler.js
@@ -4,18 +4,21 @@ private var rb:Rigidbody2D;
 private var animController:Animator; //animatino controller to get the jump
 private var sprite:SpriteRenderer;
 private var alreadyJumped:boolean = false;
+private var leftLimiter:gameObject;
+private var rightLimiter:gameObject;
 
 public var jumpHeight:int = 10;
 public var acceleration:float = 0.1;
 public var startSpeed:float = 2;
 public var maxSpeed:float = 5;
 
+
 function Start () {
 //change the players position
 	rb = gameObject.GetComponent(Rigidbody2D);
 	//Debug.Log("Hate this headache");
 	//rb.velocity.x = startSpeed;
-
+	leftLimiter = this.GetComponent(gameObject);
 	animController = this.GetComponent(Animator);
 	//when he's grounded tell the animation controller 
 	animController.SetBool("grounded", true);
@@ -44,6 +47,12 @@ function FixedUpdate () {
 
 	var frontFootHit:RaycastHit2D = Physics2D.Raycast( frontFootRay, Vector2.down, 0.01);
 	var backFootHit:RaycastHit2D = Physics2D.Raycast( backFootRay, Vector2.down, 0.01);
+
+	if(transform.position.x < leftLimiter.transform.position.x){
+		rb.velocity.x = rb.velocity.x - acceleration;
+	} else if(transform.position.x > rightLimiter.transform.position.x){ 
+		rb.velocity.x = rb.velocity.x + acceleration;
+	}
 
 
 	//Debug.Log( hit.collider);
